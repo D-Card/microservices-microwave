db73b306c2949571ced040057b1a8aeb08feb4d0
dcard <me@dcard.pt>
2023-10-18 12:17:26 +0100

    Log to Google Datastore

--- src/backend/beverageService/server.js
+++ src/backend/beverageService/server.js
@@ -1,3 +1,26 @@
+const { Datastore } = require("@google-cloud/datastore");
+
+const datastore = new Datastore();
+
+function writeToDatastore(logMessage) {
+  const logEntity = {
+    key: datastore.key(["LogEntry"]),
+    data: {
+      timestamp: new Date(),
+      message: logMessage,
+    },
+  };
+
+  datastore
+    .save(logEntity)
+    .then(() => {
+      console.log("Log entry saved to Datastore:", logMessage);
+    })
+    .catch((error) => {
+      console.error("Error writing log entry to Datastore:", error);
+    });
+}
+
 var express = require("express");
 var app = express();
 
@@ -9,12 +32,12 @@ app.use(function (req, res, next) {
 app.get("/", (req, res) => {
   const volume = parseInt(req.query["weight"]);
   const time = volume * 0.005;
-  console.log(
+  writeToDatastore(
     `A ${req.query["weight"]}ml beverage is going to be ready in ${time} minutes`
   );
   res.send({ time });
 });
 
 app.listen(80, () => {
-  console.log("beverageService listening at port 80");
+  writeToDatastore("beverageService listening at port 80");
 });
--- src/backend/potatoService/server.js
+++ src/backend/potatoService/server.js
@@ -1,3 +1,26 @@
+const { Datastore } = require("@google-cloud/datastore");
+
+const datastore = new Datastore();
+
+function writeToDatastore(logMessage) {
+  const logEntity = {
+    key: datastore.key(["LogEntry"]),
+    data: {
+      timestamp: new Date(),
+      message: logMessage,
+    },
+  };
+
+  datastore
+    .save(logEntity)
+    .then(() => {
+      console.log("Log entry saved to Datastore:", logMessage);
+    })
+    .catch((error) => {
+      console.error("Error writing log entry to Datastore:", error);
+    });
+}
+
 var express = require("express");
 var app = express();
 
@@ -9,12 +32,12 @@ app.use(function (req, res, next) {
 app.get("/", (req, res) => {
   const weight = parseInt(req.query["weight"]);
   const time = weight * 0.07;
-  console.log(
+  writeToDatastore(
     `A ${req.query["weight"]}g potato is going to be ready in ${time} minutes`
   );
   res.send({ time });
 });
 
 app.listen(80, () => {
-  console.log("potatoService listening at port 80");
+  writeToDatastore("potatoService listening at port 80");
 });
